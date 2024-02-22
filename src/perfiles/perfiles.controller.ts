4948b996df15cbd258f4739fa8a509992ddbea7f
marquitocalderon <marco.calderon1701@hotmail.com>
2024-02-21 23:56:29 -0500

    completando lo que es perfiles

--- src/perfiles/perfiles.controller.ts
+++ src/perfiles/perfiles.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Get, Post, Param, ParseIntPipe } from '@nestjs/common';
-import { CrearPerfilDto } from './dto/perfiles.dto';
+import { Body, Controller, Get, Post, Param, ParseIntPipe,Patch, Delete } from '@nestjs/common';
+import { CrearPerfilDto, UpdatePerfilDTO } from './dto/perfiles.dto';
 import { PerfilesService } from './perfiles.service';
 
 @Controller('perfiles')
@@ -15,13 +15,23 @@ export class PerfilesController {
     
     @Get(':id')
     getPerfilById(@Param('id', ParseIntPipe) id:number){
-        return this.perfilService.obterporID(id)
+        return this.perfilService.obtenerPorID(id)
     }
 
     @Post()
     postPerfiles(@Body() datosDelFrontendPerfil: CrearPerfilDto){
         return this.perfilService.insertarPerfiles(datosDelFrontendPerfil)
 
+    }
+
+    @Patch(':id')
+    actulizarPerfil(@Param('id', ParseIntPipe) id:number, @Body() datosDelFronted: UpdatePerfilDTO ){
+        return this.perfilService.actualizarPerfil(id, datosDelFronted)
+    }
+
+    @Delete(':id')
+    deletePERFIL(@Param('id', ParseIntPipe) id:number){
+        return this.perfilService.deletePerfil(id)
     }
 
 }
--- src/perfiles/perfiles.service.ts
+++ src/perfiles/perfiles.service.ts
@@ -2,7 +2,7 @@ import { Injectable , HttpException, HttpStatus} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PerfilesEntity } from './perfiles.entity';
 import { Repository } from 'typeorm';
-import { CrearPerfilDto } from './dto/perfiles.dto';
+import { CrearPerfilDto, UpdatePerfilDTO } from './dto/perfiles.dto';
 
 @Injectable()
 export class PerfilesService {
@@ -10,22 +10,31 @@ export class PerfilesService {
     constructor(@InjectRepository(PerfilesEntity) private perfilRepository: Repository<PerfilesEntity>) {}
 
    obtenerTodoslosPerfiles(){
-      return this.perfilRepository.find()
+      return this.perfilRepository.find({
+        order:{
+          id_perfil: 'DESC',
+        },
+        where:{
+          estado_perfil: true
+        }
+      })
    } 
 
-  async obterporID(id){
-
-    const PerfilEncontrado = await this.perfilRepository.findOneBy({
-       id_perfil: id,
-     })
-
-     if (!PerfilEncontrado){
-      return new HttpException('perfil no existe', HttpStatus.FOUND)
-     }
-
-    return this.perfilRepository.findOneBy({
+   async obtenerPorID(id: number) {
+    const perfilEncontrado = await this.perfilRepository.findOneBy({
       id_perfil: id,
-    })
+      estado_perfil: true
+    });
+
+    if (!perfilEncontrado) {
+      return new HttpException('Perfil no encontrado', HttpStatus.NOT_FOUND);
+    }
+
+    if (!perfilEncontrado.estado_perfil) {
+      return new HttpException('Perfil Eliminado', HttpStatus.NOT_FOUND);
+    }
+
+    return perfilEncontrado;
  } 
 
    async insertarPerfiles(perfil : CrearPerfilDto){
@@ -35,11 +44,57 @@ export class PerfilesService {
      })
 
      if (PerfilEncontrado){
-      return new HttpException('perfil ya existe', HttpStatus.CONFLICT)
+      return new HttpException('perfil ya existe en la base de datos', HttpStatus.CONFLICT)
      }
 
      const nuevoPerfil = this.perfilRepository.create(perfil)
      await this.perfilRepository.save(nuevoPerfil)
      return {perfil: "Se guardo Correctamente"}
+    }
+
+    async actualizarPerfil(id: number, perfil: UpdatePerfilDTO) {
+      const perfilExistente = await this.perfilRepository.findOneBy({
+        id_perfil: id,
+      });
+  
+      if (!perfilExistente) {
+        return new HttpException('Perfil no existe', HttpStatus.NOT_FOUND);
+      }
+  
+      // Comprobar la existencia del perfil con el mismo nombre solo si el nombre es diferente
+      if (perfil.nombre_perfil !== perfilExistente.nombre_perfil) {
+        const perfilConMismoNombre = await this.perfilRepository.findOneBy({
+          nombre_perfil: perfil.nombre_perfil,
+        });
+  
+        if (perfilConMismoNombre) {
+          return new HttpException('Perfil con el mismo nombre ya existe', HttpStatus.CONFLICT);
+        }
+      }
+  
+      // Actualizar el perfil
+      await this.perfilRepository.update(id, perfil);
+  
+      return { mensaje: 'Se actualizó correctamente' };
+    }
+
+    async deletePerfil(id: number) {
+      const perfilExistente = await this.perfilRepository.findOneBy({
+        id_perfil: id,
+        estado_perfil: true
+      });
+  
+      if (!perfilExistente) {
+        return new HttpException('Perfil no encontrado', HttpStatus.NOT_FOUND);
+      }
+
+     if (!perfilExistente.estado_perfil) {
+      return new HttpException('Perfil Eliminado', HttpStatus.NOT_FOUND);
+    }
+  
+      // Actualizar el estado del perfil a false
+      await this.perfilRepository.update(id, { estado_perfil: false });
+  
+      return { mensaje: 'Perfil eliminado exitosamente' };
     }
 }
