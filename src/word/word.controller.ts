dc8b78069ed8de0be3a1afb559e48221b7553976
Marco Calderon <marco.calderon1701@hotmail.com>
2024-02-26 18:03:50 -0500

    la fee

--- src/word/word.controller.ts
+++ src/word/word.controller.ts
@@ -1,20 +1,28 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Header, Post, Res } from '@nestjs/common';
 import { WordService } from './word.service';
 import { GenerarWordDto } from './dto/generaword.dto';
+import { Response } from 'express';
 
 @Controller('word')
 export class WordController {
     
     constructor(private wordService: WordService){}
 
-
     @Get()
     holaword(){
-        return "hola estoy compilandosafsaf"
+        return "hola estoy compilandosafsaf";
     }
 
     @Post()
-    generarWord(@Body() datosDelFrontend: GenerarWordDto){
-        return this.wordService.generateWord(datosDelFrontend)
+    @Header('Access-Control-Expose-Headers', 'Content-Disposition')
+    async generarWord(@Body() datosDelFrontend: GenerarWordDto, @Res() res: Response) {
+        const documentoword = this.wordService.generateWord(datosDelFrontend);
+
+        // Set the headers
+        res.setHeader('Content-Type', 'application/msword');
+        res.setHeader('Content-Disposition', `vengodenestjs`);
+
+        // Send the Word document as the response
+        res.send(documentoword);
     }
 }
--- src/word/word.service.ts
+++ src/word/word.service.ts
@@ -5,7 +5,7 @@ import * as path from 'path';
 
 import * as PizZip from 'pizzip';
 import * as Docxtemplater from 'docxtemplater';
-import ImageModule from 'docxtemplater-image-module-pwndoc';
+import * as ImageModule  from 'docxtemplater-image-module-pwndoc';
 import { GenerarWordDto } from './dto/generaword.dto';
 
 @Injectable()
@@ -16,15 +16,52 @@ export class WordService {
         // Define una ruta relativa al archivo y Lee el contenido binario del archivo de plantilla .docx
         const content = fs.readFileSync(
 
-            // EN DESARROLO LOCAL
-            // path.resolve(__dirname, '../../public/docs/prueba.docx'),
+            // // EN DESARROLO LOCAL
+            // path.resolve(__dirname, '../../public/docs/prueba.docx'),'binary' 
 
             // EN PRODUCCION YA PARA SUBIR
             path.resolve(__dirname, '../public/docs/prueba.docx'),
             'binary'
         );
 
-        console.log(content, "hola bb");
+        // const imagen1 = path.resolve(__dirname, '../../public/imagenes/marco.jpg');
+
+        const imagen1 = path.resolve(__dirname, '../public/imagenes/marco.jpg');
+
+        const zip = new PizZip(content);
+        const imageOptions = {
+            centered: false,
+            getImage(tagValue, tagName) {
+                console.log({ tagValue, tagName });
+                return fs.readFileSync(tagValue);
+            },
+            getSize() {
+                // it also is possible to return a size in centimeters, like this : return [ "2cm", "3cm" ];
+                return [500, 500];
+            },
+        };
+
+
+        // Crea una instancia de Docxtemplater, configurando opciones como 'paragraphLoop' y 'linebreaks'
+        const doc = new Docxtemplater(zip, {
+            modules: [new ImageModule(imageOptions)],
+        });
+
+
+        doc.render({
+            nombre: datosDelFrontend.nombre,
+            edad: datosDelFrontend.edad,
+            direccion: datosDelFrontend.direccion,
+            'foto': imagen1
+    
+        });
+
+        const documentoword = doc.getZip().generate({
+            type: 'nodebuffer',
+            compression: 'DEFLATE',
+        });
+
+        return documentoword
     }
 
 
